e40877b35432d49cda470dcc935f6f7f9d77e1c1
ccrusset <ccrusset@gmail.com>
2020-02-22 14:54:25 -0400

    prueba003

--- Prueba01/src/sdfsf.java
+++ Prueba01/src/sdfsf.java
@@ -4,7 +4,7 @@ public class sdfsf {
     public static void main (String[]args){
 
         String xPath = "C:\\Users\\Carolina Crusset\\Documents\\ChromeDriver";
-        System.setProperties();
+
 
     }
 }
